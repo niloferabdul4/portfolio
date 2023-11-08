f9557359066cd86eafdc7e8a3574940d18aacd03
niloferabdul4 <niloferabdul4@gmail.com>
2023-11-08 15:56:57 +0400

    commit

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -4,7 +4,7 @@ import './about.css'
 const About = () => {
     return (
         <div>
-            <div className="about_container" id='About'>                
+            <div className="about_container" id='About Me'>                
                         <h2 className='about_heading'>About Me</h2>
                         <div className='about_box'>                            
                             <p> I'm a <b>Frontend Web Developer</b> building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the Projects section.</p>
