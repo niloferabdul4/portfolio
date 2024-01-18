c19605811723952db9b7b3fec170a3dcb7a9b3d0
niloferabdul4 <niloferabdul4@gmail.com>
2024-01-18 11:33:14 +0400

    commit

--- src/Components/Projects/projects_data.js
+++ src/Components/Projects/projects_data.js
@@ -4,90 +4,113 @@ import recipe from '../../assets/recipes.png'
 import youtube from '../../assets/youtube.png'
 import quiz from '../../assets/quiz.png'
 import tenzies from '../../assets/tenzies.jpg'
-import portfolio from '../../assets/portfolio.png'
 import realestate from '../../assets/properties.png'
 import notes from '../../assets/notes.png'
+import stackoverflow from '../../assets/stackoverflow.png'
+import chatapp from '../../assets/chat-app.png'
+import restaurant from '../../assets/restaurant-landing-page.png'
 
  const projects_data = [
+
     {
-    id:1,
-    title:'E-Commerce App',
-    description:' A fully responsive e-commerce web app.Front-end made with HTML, CSS, React core concepts like Hooks, Context API, and Styled Components Integrated Firebase for Authentication and database. Added functionality such as add to cart, filters, fetch API, etc',
-    url: 'https://github.com/niloferabdul4/eshop.git', 
+        id:1,
+        title:'Stack Overflow Clone',
+        description:'Built using MERN Stack.Introduced a chatbot feature using OpenAI API and subscription plans using Stripe Payment Gateway and user rewards system.',
+        github:'https://github.com/niloferabdul4/stack-overflow-frontend.git',
+        demo:'https://stackoverflow-clone-fullstack.netlify.app/'  ,
+        image:stackoverflow
+      
+    },
+
+    {
+    id:2,
+    title:'E-Commerce ',
+    description:'Built an eCommerce app with React.js. Integrated Firebase for authentication.Added features like product filtering,calculate total amount etc',
+    github: 'https://github.com/niloferabdul4/eshop.git', 
     demo:'https://ecommerce-app-fac17.web.app/'  ,
     image:ecommerce
   
-},{
-    id:2,
+},
+
+{
+    id:3,
+    title:'Real-Time Chat App',
+    description: 'Created a private one-to-one chat application using React.js, incorporating Firebase for secure database management and authentication.',
+    github: 'https://github.com/niloferabdul4/chat_app.git',
+    demo:'https://chat-app-2891c.web.app/',
+    image:chatapp
+   
+},
+{
+    id:4,
     title:'Social Media Clone App ',
-    description: 'Built using HTML and React JS core Concepts like Hooks, Router, Context API and styled components.Integrated Firebase for Google Authentication and database. Added functionality such as like, comment, upload files, etc',
-    url: 'https://github.com/niloferabdul4/facebookclone.git',
+    description: 'Built a social media app in React.js with Firebase for authentication and database management. Added functionalities like likes,comment etc',
+    github: 'https://github.com/niloferabdul4/facebookclone.git',
     demo:'https://socialmediaapp-d705d.web.app',
     image:facebook1
  
 },
 {
-    id:3,
-    title:'Youtube Clone App',
-    description: 'Fully Responsive YouTube clone app built Using React Material UI. Integrated Rapid API for TouTube data. Display the videos based on category and also show the related videos to that particular videos.',
-    url: 'https://github.com/niloferabdul4/youtube_clone.git',
+    id:5,
+    title:'Youtube Clone',
+    description: 'Developed a YouTube clone using React.js and Material-UI. Integrated Firebase for authentication and Rapid API for fetching data.',
+    github: 'https://github.com/niloferabdul4/youtube_clone.git',
     demo:'https://youtube-clone-livid-two.vercel.app/',
     image:youtube
    
 },
 
-{
-    id:4,
-    title:'Personal Portfolio',
-    description: 'Implemented using React Js core topics like Context API React Hooks,HTML and CSS',
-    url: 'https://github.com/niloferabdul4/portfolio.git',
-    demo:'https://personal-portfolio-e7c9e.web.app',
-    image:portfolio
-   
-},
-
 
 {
-    id:5,
-    title:'Real-Estate App',
-    description: 'Implemented using React Js core topics like Context API React Hooks,HTML and CSS',
-    url: 'https://github.com/niloferabdul4/real-estate-app.git',
+    id:6,
+    title:'Real-Estate',
+    description: 'Built a Real Estate Webpage in React.js with Firebase for authentication and database management. Added functionalities like save,filtering etc',
+    github: 'https://github.com/niloferabdul4/real-estate-app.git',
     demo:'https://realestateapp-dd755.web.app/',
     image:realestate
    
 },
 {
-    id:6,
-    title:'Recipe Finder App',
+    id:7,
+    title:'Recipe Finder',
     description: 'Implemented using React Js core topics like Context API React Hooks,HTML and CSS',
-    url: 'https://github.com/niloferabdul4/recipeapp.git',
+    github: 'https://github.com/niloferabdul4/recipeapp.git',
     demo:'https://recipefinder-app.netlify.app/',
     image:recipe
    
 },
 
 {
-    id:7,
+    id:8,
+    title:'Restaurant Landing Page',
+    description: 'Implemented using React Js core topics like Context API React Hooks,HTML and CSS',
+    github: 'https://github.com/niloferabdul4/restaurant-landing-page.git',
+    demo:'https://restaurant-landing-web-page.netlify.app/',
+    image:restaurant
+   
+},
+{
+    id:9,
     title:'Quiz App',
     description: 'Built using React Js core topics like Context API React Hooks',
-    url: 'https://github.com/niloferabdul4/quiz_app.git',
+    github: 'https://github.com/niloferabdul4/quiz_app.git',
     demo:'https://quizes-123.netlify.app/',
     image:quiz
   
 },{
-    id:8,
+    id:10,
     title:'Tenzies Game',
     description: 'Built using React Js,HTML and  CSS',
-    url: 'https://github.com/niloferabdul4/tenziesgame.git',
+    github: 'https://github.com/niloferabdul4/tenziesgame.git',
     demo:'https://tenzies-game-dfaaf.web.app',
     image:tenzies
  
 },
 {
-    id:9,
+    id:11,
     title:'Notes App',
     description:'Built using React Js,HTML and  CSS',
-    url:'https://github.com/niloferabdul4/easynotesapp.git',
+    github:'https://github.com/niloferabdul4/easynotesapp.git',
     demo:'https://notesapp-4ca19.web.app/',
     image:notes
     
